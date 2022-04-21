18046277ed9ecf06fedf69b9fd51a1b52f15e10f
appumohanan <80033796+appumohanan@users.noreply.github.com>
2022-04-22 00:10:19 +0530

    Add files via upload

---
+++ 4_TestCases/door sensor.c
@@ -0,0 +1,22 @@
+/*
+ * door sensor.c
+ *
+ * Created: 20-04-2022 9.01.54 AM
+ * Author : hai
+ */ 
+
+#include <avr/io.h>
+int main(void)
+{
+	DDRB=DDRB&0b11111101;//fd
+	DDRC=DDRC|0b01000000;//40
+	while(1)
+	{
+		if(PINB & 0b00000010)//02
+		PORTC=PORTC|0b01000000;//40
+		else
+		PORTC=PORTC&0b10111111;//bf
+	}
+	return 0;
+}
+
